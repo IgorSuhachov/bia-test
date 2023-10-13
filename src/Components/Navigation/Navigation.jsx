0ca9f85d7acc2022a81f0fb1d7061a9eab1f8c42
Игорь Сухачёв <42006386+IgorSuhachov@users.noreply.github.com>
2023-10-14 00:27:05 +0300

    feat: Сверстал навигацию

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -4,7 +4,55 @@ import Logo from '../../images/logo.svg';
 export default function Navigation() {
   return (
     <div className="navigation">
-      <img src={Logo} alt="логотип" className="navigation__logo" />
+      <div className="navigation__logo">
+        <img src={Logo} alt="логотип" />
+        <p className="navigation__text">tmc</p>
+      </div>
+
+      <div className="nav__top">
+        <ul className="navigation__elements">
+          <li className="navigation__element">
+            <button className="navigation__button navigation__button_active">
+              <i className="ri-calendar-check-line ri-xl"></i>
+            </button>
+          </li>
+          <li className="navigation__element">
+            <button className="navigation__button">
+              <i class="ri-booklet-line ri-xl"></i>
+            </button>
+          </li>
+          <li className="navigation__element">
+            <button className="navigation__button">
+              <i className="ri-truck-line ri-xl"></i>
+            </button>
+          </li>
+          <li className="navigation__element">
+            <button className="navigation__button ">
+              <i className="ri-admin-line ri-xl"></i>
+            </button>
+          </li>
+          <li className="navigation__element">
+            <button className="navigation__button">
+              <i className="ri-briefcase-line ri-xl"></i>
+            </button>
+          </li>
+          <li className="navigation__element">
+            <button className="navigation__button">
+              <i className="ri-wechat-line ri-xl"></i>
+            </button>
+          </li>
+          <li className="navigation__element">
+            <button className="navigation__button">
+              <i className="ri-alert-line ri-xl"></i>
+            </button>
+          </li>
+        </ul>
+      </div>
+      <div className="nav__bot">
+        <button className="navigation__button">
+          <i class="ri-arrow-right-circle-line ri-xl"></i>
+        </button>
+      </div>
     </div>
   );
 }
