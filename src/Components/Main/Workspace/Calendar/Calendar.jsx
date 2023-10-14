9b6e2d61f390f4ad360803e1ce80166946a6a38d
Игорь Сухачёв <42006386+IgorSuhachov@users.noreply.github.com>
2023-10-14 19:36:52 +0300

    fix: Исправил расположение элементов
    Элементы в Workspace в верхней части были в неправильном контейнере

---
+++ src/Components/Main/Workspace/Calendar/Calendar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Calendar() {
+  return (
+    <section className="calendar">
+      <div>Calendar</div>
+    </section>
+  );
+}
--- src/Components/Main/Workspace/Workspace.jsx
+++ src/Components/Main/Workspace/Workspace.jsx
@@ -1,26 +1,40 @@
 import React from 'react';
+import Calendar from './Calendar/Calendar';
 
 export default function Workspace() {
   return (
     <section className="workspace">
-      <div className="workspace__wrapper">
-        <input type="text" placeholder="Поиск" className="workspace__input" />
-        <button className="workspace__filters">Фильтры</button>
+      <div className="workspace__heading">
+        <div className="workspace__wrapper">
+          <input type="text" placeholder="Поиск" className="workspace__input" />
+          <button className="workspace__filters">Фильтры</button>
+        </div>
+        <div className="workspace__wrapper">
+          <select name="" id="">
+            <option value="">Январь</option>
+            <option value="">Февраль</option>
+            <option value="">Март</option>
+            <option value="">Апрель</option>
+            <option value="">Май</option>
+            <option value="">Июнь</option>
+            <option value="">Июль</option>
+            <option value="">Август</option>
+            <option value="">Сентябрь</option>
+            <option value="">Октябрь</option>
+            <option value="">Ноябрь</option>
+            <option value="">Декабрь</option>
+          </select>
+          <select name="" id="">
+            <option value="">Москва</option>
+            <option value="">Санкт-Петербург</option>
+            <option value="">Казань</option>
+            <option value="">Сочи</option>
+            <option value="">Волгоград</option>
+            <option value="">Новосибирск</option>
+          </select>
+        </div>
       </div>
-      <div className="workspace__wrapper">
-        <select name="" id="">
-          <option value="">Апрель</option>
-          <option value="">Апрель</option>
-          <option value="">Апрель</option>
-          <option value="">Апрель</option>
-        </select>
-        <select name="" id="">
-          <option value="">Апрель</option>
-          <option value="">Апрель</option>
-          <option value="">Апрель</option>
-          <option value="">Апрель</option>
-        </select>
-      </div>
+      <Calendar />
     </section>
   );
 }
